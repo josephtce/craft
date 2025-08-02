cad457ae839ba2e58ed62afb47057262578296fb
josephtce <136669128+josephtce@users.noreply.github.com>
2025-08-02 13:06:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 #
+import random
+
+with open("projects.txt", "r", encoding="utf-8") as f:
+    projects = [line.strip() for line in f if line.strip()]
+
+print("Dự án ngẫu nhiên:", random.choice(projects))
