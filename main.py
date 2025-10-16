b5fc27323fef97a0c692252ad84bf7b0a52b6913
josephtce <136669128+josephtce@users.noreply.github.com>
2025-10-16 09:37:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import random
 with open("projects.txt", "r", encoding="utf-8") as f:
     projects = [line.strip() for line in f if line.strip()]
 
-print("Dự án ngẫu nhiên:", random.choice(projects))--06--09--10--11--12--13--16--21--22--23--24--26--27--31--01--03--04--07--09--10--11--13--14--19--20--21--23--27--01--02--05--08--09
+print("Dự án ngẫu nhiên:", random.choice(projects))--06--09--10--11--12--13--16--21--22--23--24--26--27--31--01--03--04--07--09--10--11--13--14--19--20--21--23--27--01--02--05--08--09--16
